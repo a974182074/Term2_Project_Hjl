679d6eaf9fade427bf313ac607c26247c53fa53b
a974182074 <974182074@qq.com>
2020-11-27 14:52:19 +0800

    ch7作业

--- trunk/js/index.js
+++ trunk/js/index.js
@@ -1,10 +1,10 @@
 /**
  * 乐购商城首页
- * 2020-10-01 萝卜
+ * 2020-10-01 
  */
 $(function(){
     /*首页大图轮播*/
-    $("#banner").tyslide({
+    $("#bannerInner").tyslide({
         boxh:460,//盒子的高度
         w:1000,//盒子的宽度
         h:390,//图片的高度
@@ -110,4 +110,43 @@ $(function(){
             scrollTop:0
         },300)
     })
-});
+    /*二维码滑出效果 */
+    $('.qr-code .ticket').hover(function (){
+        //让二维码出来
+        $('.qr-code div').stop(true).animate({
+            left:'-100px'
+        })
+    }, function(){
+        //让二维码收回去
+        $('.qr-code div').stop(true).animate({
+            left:0
+    })
+   })
+
+   /*顶部搜索框 */
+   $(document).scroll(function(){
+       //获取到顶部的距离
+       var topDistance = $('html, body').scrollTop();
+       //判断
+       if(topDistance > 500){
+           //如果滚动距离大于500  滑下来
+           $('.top-search-box').slideDown(300)
+       }else{
+           //否则  收回去
+           $('.top-search-box').slideUp(300)
+       }
+})
+
+    /*楼梯跳转 */
+    $('.floor li').click(function() {
+       //获取索引
+       var index = $(this).index();
+       //选中每一个板块到顶部的偏移
+       var topOffset =$('.floorBox').eq(index).offset().top;
+       
+       //让滚动条滚到这个位置
+       $('html, body').animate({
+           scrollTop:topOffset  -50
+       })
+    })
+})
